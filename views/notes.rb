871c294ce320a8ca1872756e44e1fd3e6c46cfde
Eisenheart <felixlobkowicz@gmail.com>
2015-07-26 15:21:17 -0700

    simple user stories

---
+++ views/notes.rb
@@ -0,0 +1,16 @@
+=begin
+#I need to have an overview over each cohort. Otherwise chaos.
+#Cohort
+- one cohort, 500 soldiers approx, 500 mules accross a legion, 5 centurions(legion is 5200 solider approx. max.)
+- want to know name and experience of each soldier, centurion and senior centurion
+- want to know how many soldiers each centurions commands
+- want to delete soldiers from cohort
+- want to promote(perhaps demote) soldiers to centurion
+- want to add soldiers
+
+Associations:
+- one centurions has many soldiers
+- one cohort has has many soldiers
+- one cohort has has many centurion
+- one cohort has many mules and a mule has many cohorts
+=end
